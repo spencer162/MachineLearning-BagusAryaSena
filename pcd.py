ad789b9c8c70b0577594552c3ed4e3bbdaa77822
spencer162 <bagusarya164@gmail.com>
2025-10-24 23:20:56 +0700

    Add files via upload

---
+++ pcd.py
@@ -0,0 +1,18 @@
+a = 21
+b = 36
+
+jumlah = a + b
+bagi = a / b
+kurang = a - b
+kali = a * b
+pangkat = a ** b
+sisa_bagi = a % b
+pembagian_bulat = a // b
+
+print("Hasil dari" , a, "+", b, "adalah:", jumlah)
+print("Hasil dari" , a, "/", b, "adalah:", bagi)
+print("Hasil dari" , a, "-", b, "adalah:", kurang)
+print("Hasil dari" , a, "*", b, "adalah:", kali)
+print("Hasil dari" , a, "**", b, "adalah:", pangkat)
+print("Hasil dari" , a, "%", b, "adalah:", sisa_bagi)
+print("Hasil dari" , a, "//", b, "adalah:", pembagian_bulat)
